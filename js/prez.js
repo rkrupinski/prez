786636170100eb0c954b3a4db6fcf2a64afe402c
Rafał Krupiński <krupinski.rafal@gmail.com>
2013-03-18 19:17:05 +0100

    Webkit only + customizable per-slide transition-duration

--- js/prez.js
+++ js/prez.js
@@ -2,11 +2,12 @@ define(["utils"], function(utils) {
     "use strict";
 
     var defaults = {
-        basePerspective     : 1000,
-        activeClassName     : "prez-active",
-        currentClassName    : "prez-current",
-        eventNamespace      : "prez",
-        transitionEndEvents : ["webkitTransitionEnd", "transitionend"]
+        basePerspective        : 1000,
+        activeClassName        : "prez-active",
+        currentClassName       : "prez-current",
+        transitionEndEvent     : "webkitTransitionEnd",
+        transitionDurationProp : "WebkitTransitionDuration",
+        transformProp          : "WebkitTransform"
     };
 
     function Prez(config) {
@@ -28,46 +29,35 @@ define(["utils"], function(utils) {
         Array.prototype.forEach.call(this._slides, function(slide) {
             data = slide.dataset;
 
-            this._setTransform(
-                slide,
-                [
-                    "translate(-50%, -50%) ",
-                    "translate3d(" + data.posx + "px, " + data.posy + "px, " + data.posz + "px) ",
-                    "rotateX(" + data.rotatex + "deg) ",
-                    "rotateY(" + data.rotatey + "deg) ",
-                    "rotateZ(" + data.rotatez + "deg) ",
-                    "scale(" + data.scale + ")"
-                ].join("")
-            );
+            slide.style[this._config.transformProp] = [
+                "translate(-50%, -50%) ",
+                "translate3d(" + data.posx + "px, " + data.posy + "px, " + data.posz + "px) ",
+                "rotateX(" + data.rotatex + "deg) ",
+                "rotateY(" + data.rotatey + "deg) ",
+                "rotateZ(" + data.rotatez + "deg) ",
+                "scale(" + data.scale + ")"
+            ].join("");
+
         }.bind(this));
     };
 
     Prez.prototype._bindHandlers = function() {
-        this._config.transitionEndEvents.forEach(function(event) {
-            this._prez2.addEventListener(event, this._switch.bind(this), false);
-        }.bind(this));
+        this._prez2.addEventListener(this._config.transitionEndEvent, this._switch.bind(this), false);
     };
 
     Prez.prototype._getCurrentSlide = function() {
         return this._slides[this._current];
     };
 
-    Prez.prototype._setTransform = function(el, transform) {
-        el.style.WebkitTransform = transform;
-        el.style.transform       = transform;
-    };
-
     Prez.prototype._switch = function(e) {
-        var data;
-
         if (e.target !== this._prez2) {
             return;
         }
 
-        data = this._getCurrentSlide().dataset;
-
-        setTimeout(this[this._current === this._slides.length - 1 ? '_end' : '_next'].bind(this), data.duration);
-
+        setTimeout(
+            this[this._current < this._slides.length - 1 ? '_next' : '_end'].bind(this),
+            this._getCurrentSlide().dataset.duration
+        );
     };
 
     Prez.prototype._next = function() {
@@ -82,23 +72,21 @@ define(["utils"], function(utils) {
 
         slide.classList.add(this._config.currentClassName);
 
-        this._setTransform(
-            this._prez1,
-            [
-                "perspective(" + this._config.basePerspective + "px) ",
-                "scale(" + (1 / data.scale) + ")"
-            ].join('')
-        );
+        this._prez1.style[this._config.transformProp] = [
+            "perspective(" + this._config.basePerspective + "px) ",
+            "scale(" + (1 / data.scale) + ")"
+        ].join("");
 
-        this._setTransform(
-            this._prez2,
-            [
-                "rotateZ(" + -data.rotatez + "deg) ",
-                "rotateY(" + -data.rotatey + "deg) ", 
-                "rotateX(" + -data.rotatex + "deg) ",
-                "translate3d(" + -data.posx + "px, " + -data.posy + "px, " + -data.posz + "px)"
-            ].join("")
-        );
+        this._prez1.style[this._config.transitionDurationProp] = data.transition + "ms";
+
+        this._prez2.style[this._config.transformProp] = [
+            "rotateZ(" + -data.rotatez + "deg) ",
+            "rotateY(" + -data.rotatey + "deg) ", 
+            "rotateX(" + -data.rotatex + "deg) ",
+            "translate3d(" + -data.posx + "px, " + -data.posy + "px, " + -data.posz + "px)"
+        ].join("");
+
+        this._prez2.style[this._config.transitionDurationProp] = data.transition + "ms";
     };
 
     Prez.prototype._end = function() {
