66f604dd15258845831bd9842bfb82f6e1adbae5
Rafał Krupiński <krupinski.rafal@gmail.com>
2013-04-02 18:50:27 +0200

    Redundancy

--- js/prez.js
+++ js/prez.js
@@ -18,117 +18,121 @@ define(["utils", "config"], function(utils, config) {
 		this._bindHandlers();
 	}
 
-	Prez.prototype._prepareSlides = function() {
-		var data;
+	Prez.prototype = {
 
-		this._slides.forEach(function(slide) {
-			data = slide.dataset;
+		_prepareSlides: function() {
+			var data;
 
-			slide.classList.add(this._config.beforeClassName);
+			this._slides.forEach(function(slide) {
+				data = slide.dataset;
 
-			slide.style[this._config.transformProp] = [
-				"translate(-50%, -50%) ", 
-				"translate3d(",
-					data.posX || 0, "px, ",
-					data.posY || 0, "px, ",
-					data.posZ || 0, "px",
-				")",
-				"rotateX(", data.rotateX || 0, "deg) ",
-				"rotateY(", data.rotateY || 0, "deg) ",
-				"rotateZ(", data.rotateZ || 0, "deg) ",
-				"scale(", data.scale || 1, ")"
+				slide.classList.add(this._config.beforeClassName);
+
+				slide.style[this._config.transformProp] = [
+					"translate(-50%, -50%) ", 
+					"translate3d(",
+						data.posX || 0, "px, ",
+						data.posY || 0, "px, ",
+						data.posZ || 0, "px",
+					")",
+					"rotateX(", data.rotateX || 0, "deg) ",
+					"rotateY(", data.rotateY || 0, "deg) ",
+					"rotateZ(", data.rotateZ || 0, "deg) ",
+					"scale(", data.scale || 1, ")"
+				].join("");
+
+			}.bind(this));
+		},
+
+		_bindHandlers: function() {
+			this._prez2.addEventListener(this._config.transitionEndEvent, this._switch.bind(this), false);
+		},
+
+		_getCurrentSlide: function() {
+			return this._slides[this._current];
+		},
+
+		_isLast: function() {
+			return this._current === this._slides.length - 1;
+		},
+
+		_switch: function(e) {
+			var slide;
+
+			if (e.target !== this._prez2) {
+				return;
+			}
+
+			slide = this._getCurrentSlide();
+
+			slide.classList.remove(this._config.beforeClassName);
+			slide.classList.remove(this._config.nextClassName);
+			slide.classList.add(this._config.currentClassName);
+
+			setTimeout(
+				this[this._isLast() ? "_end" : "_next"].bind(this),
+				this._getCurrentSlide().dataset.duration || this._config.defaults.lifetime
+			);
+		},
+
+		_next: function() {
+			var slide = this._getCurrentSlide();
+
+			slide.classList.remove(this._config.currentClassName);
+			slide.classList.add(this._config.afterClassName);
+
+			this._current += 1;
+			this._transition();
+		},
+
+		_transition: function() {
+			var slide = this._getCurrentSlide(),
+				data = slide.dataset;
+
+			slide.classList.add(this._config.nextClassName);
+
+			this._prez1.style[this._config.transformProp] = [
+				"perspective(", this._config.defaults.perspective, "px) ",
+				"scale(", 1 / data.scale, ")"
 			].join("");
 
-		}.bind(this));
-	};
+			this._prez1.style[this._config.transitionDurationProp] = (data.transition || this._config.defaults.transition) + "ms";
+			this._prez1.style[this._config.transitionTimingProp] = data.easing || this._config.defaults.easing;
 
-	Prez.prototype._bindHandlers = function() {
-		this._prez2.addEventListener(this._config.transitionEndEvent, this._switch.bind(this), false);
-	};
+			this._prez2.style[this._config.transformProp] = [
+				"rotateZ(",	-data.rotateZ || 0, "deg) ",
+				"rotateY(", -data.rotateY || 0, "deg) ",
+				"rotateX(", -data.rotateX || 0, "deg) ",
+				"translate3d(",
+					-data.posX || 0, "px, ",
+					-data.posY || 0, "px, ",
+					-data.posZ || 0, "px",
+				")"
+			].join("");
 
-	Prez.prototype._getCurrentSlide = function() {
-		return this._slides[this._current];
-	};
+			this._prez2.style[this._config.transitionDurationProp] = (data.transition || this._config.defaults.transition) + "ms";
+			this._prez2.style[this._config.transitionTimingProp] = data.easing || this._config.defaults.easing;
+		},
 
-	Prez.prototype._isLast = function() {
-		return this._current === this._slides.length - 1;
-	};
+		_end: function() {
+			var slide = this._getCurrentSlide();
 
-	Prez.prototype._switch = function(e) {
-		var slide;
+			slide.classList.remove(this._config.currentClassName);
+			slide.classList.add(this._config.afterClassName);
 
-		if (e.target !== this._prez2) {
-			return;
+			if (typeof this._config.callback === "function") {
+				this._config.callback.call(this, Date.now() - this._startTime);
+			}
+		},
+
+		start: function() {
+			document.body.classList.add(this._config.activeClassName);
+
+			this._startTime = Date.now();
+			this._current = 0;
+			this._transition();
 		}
 
-		slide = this._getCurrentSlide();
-
-		slide.classList.remove(this._config.beforeClassName);
-		slide.classList.remove(this._config.nextClassName);
-		slide.classList.add(this._config.currentClassName);
-
-		setTimeout(
-			this[this._isLast() ? "_end" : "_next"].bind(this),
-			this._getCurrentSlide().dataset.duration || this._config.defaults.lifetime
-		);
-	};
-
-	Prez.prototype._next = function() {
-		var slide = this._getCurrentSlide();
-
-		slide.classList.remove(this._config.currentClassName);
-		slide.classList.add(this._config.afterClassName);
-
-		this._current += 1;
-		this._transition();
-	};
-
-	Prez.prototype._transition = function() {
-		var slide = this._getCurrentSlide(),
-			data = slide.dataset;
-
-		slide.classList.add(this._config.nextClassName);
-
-		this._prez1.style[this._config.transformProp] = [
-			"perspective(", this._config.defaults.perspective, "px) ",
-			"scale(", 1 / data.scale, ")"
-		].join("");
-
-		this._prez1.style[this._config.transitionDurationProp] = (data.transition || this._config.defaults.transition) + "ms";
-		this._prez1.style[this._config.transitionTimingProp] = data.easing || this._config.defaults.easing;
-
-		this._prez2.style[this._config.transformProp] = [
-			"rotateZ(",	-data.rotateZ || 0, "deg) ",
-			"rotateY(", -data.rotateY || 0, "deg) ",
-			"rotateX(", -data.rotateX || 0, "deg) ",
-			"translate3d(",
-				-data.posX || 0, "px, ",
-				-data.posY || 0, "px, ",
-				-data.posZ || 0, "px",
-			")"
-		].join("");
-
-		this._prez2.style[this._config.transitionDurationProp] = (data.transition || this._config.defaults.transition) + "ms";
-		this._prez2.style[this._config.transitionTimingProp] = data.easing || this._config.defaults.easing;
-	};
-
-	Prez.prototype._end = function() {
-		var slide = this._getCurrentSlide();
-
-		slide.classList.remove(this._config.currentClassName);
-		slide.classList.add(this._config.afterClassName);
-
-		if (typeof this._config.callback === "function") {
-			this._config.callback.call(this, Date.now() - this._startTime);
-		}
-	};
-
-	Prez.prototype.start = function() {
-		document.body.classList.add(this._config.activeClassName);
-
-		this._startTime = Date.now();
-		this._current = 0;
-		this._transition();
 	};
 
 	return Prez;
